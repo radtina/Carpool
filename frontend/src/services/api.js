b35679415ce97066473720c48ec2a1d1f332af01
Tina Radvar <radvar.tina41@gmail.com>
2025-03-11 22:55:23 +0100

    build

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://carpool-85ok.onrender.com/', // Adjust if your backend runs on a different URL or port
+  baseURL: 'https://carpoolbackend-hj1i.onrender.com', // Adjust if your backend runs on a different URL or port
 });
 
 export default api;
